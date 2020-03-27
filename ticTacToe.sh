8c32ffc08ef4a1fc626acd4ad72cb4234f952bca
shubhamdeulkar27 <shubhamdeulkar27@gmail.com>
2020-03-27 11:36:00 +0530

    Refactored: Assigning the Letter O or X to The Player

--- ticTacToe.sh
+++ ticTacToe.sh
@@ -1,6 +1,24 @@
 #!/bin/bash -x
 
+#FUNCTION TO RESET THE BOARD
 function reset(){
 	board=(. . . . . . . . .)
 }
+
+#FUNCTION TO ASSIGN LETTER TO THE PLAYER
+function assign(){
+	random=$(( RANDOM%2 ))
+	if (( $random==0 ))
+	then
+		PLAYER="X"
+		CPU="O"
+	else
+		PLAYER="O"
+		CPU="X"
+	fi
+}
+
 reset
+assign
+echo $PLAYER
+echo $CPU
