6c7fe3ad31c6db816249efc32150fd60b3ca81e6
shubhamdeulkar27 <shubhamdeulkar27@gmail.com>
2020-03-27 12:04:16 +0530

    Refactored: Showing The Board To The Player

--- ticTacToe.sh
+++ ticTacToe.sh
@@ -29,6 +29,15 @@ function toss(){
 	fi
 }
 
+#FUNCTION TO PRINT BOARD
+function print(){
+	echo "r\c 0 1 2"
+	echo "0   ${board[0]} ${board[1]} ${board[2]}"
+	echo "1   ${board[3]} ${board[4]} ${board[5]}"
+	echo "2   ${board[6]} ${board[7]} ${board[8]}"
+}
+
 reset
 assign
 toss
+print
